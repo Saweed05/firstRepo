06462586717935946cfa94dd12e36ababfff1ee6
Saweed Hussain <saweed411@gmail.com>
2022-01-19 03:19:54 +0500

    my first commit

---
+++ First_Repositary/Data_visualtion_02.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+my_data=pd.read_csv("data02.csv")
+
+sns.set_theme(style="ticks",palette="muted",color_codes=True)
+sns.countplot(x="Gender",data=my_data, hue="Location")
+plt.title("Data Visualization with Baba_Ammar")
+plt.show()
